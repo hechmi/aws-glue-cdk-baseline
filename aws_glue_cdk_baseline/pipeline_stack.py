4b08d63707295545ab335b558392a3345cf59a52
Hechmi Khelifi <hechmik@amazon.com>
2025-10-18 22:43:20 -0400

    Fix Node.js compatibility by specifying STANDARD_7_0 build environment

--- aws_glue_cdk_baseline/pipeline_stack.py
+++ aws_glue_cdk_baseline/pipeline_stack.py
@@ -1,171 +1,90 @@
-# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
 from typing import Dict
+import aws_cdk as cdk
 from aws_cdk import (
-    Environment,
     Stack,
-    aws_codecommit as codecommit,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_codebuild as codebuild
 )
 from constructs import Construct
-from aws_cdk.pipelines import CodePipeline, CodePipelineSource, CodeBuildStep, ManualApprovalStep, ShellStep
-from aws_cdk import aws_codebuild as codebuild
-from aws_cdk import pipelines
-from helper import create_archive
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, CodeBuildStep
 from aws_glue_cdk_baseline.glue_app_stage import GlueAppStage
-
-
+ 
+GITHUB_REPO = "hechmi/aws-glue-cdk-baseline"
+GITHUB_BRANCH = "main"
+GITHUB_CONNECTION_ARN = "arn:aws:codeconnections:us-east-1:009507777973:connection/d768c13d-4e9f-499a-be0a-52644fa2ad44"
+ 
 class PipelineStack(Stack):
-
+ 
     def __init__(self, scope: Construct, construct_id: str, config: Dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        archive_file = create_archive()
-
-        codecommit_repo = codecommit.Repository(
-            self, "GitRepo",
-            repository_name="aws-glue-cdk-baseline",
-            code=codecommit.Code.from_zip_file(
-                file_path=archive_file,
-                branch="main"
-            )
+ 
+        source = CodePipelineSource.connection(
+            GITHUB_REPO,
+            GITHUB_BRANCH,
+            connection_arn=GITHUB_CONNECTION_ARN
         )
-
-        i_codecommit_repo = codecommit.Repository.from_repository_name(
-            self, "IGlueRepo",
-            codecommit_repo.repository_name
-        )
-
-        source = CodePipelineSource.code_commit(
-            repository=i_codecommit_repo,
-            branch="main"
-        )
+ 
         pipeline = CodePipeline(self, "GluePipeline",
             pipeline_name="GluePipeline",
             cross_account_keys=True,
             docker_enabled_for_synth=True,
-            code_build_defaults=pipelines.CodeBuildOptions(
-                build_environment=codebuild.BuildEnvironment(
-                    build_image=codebuild.LinuxBuildImage.STANDARD_7_0
-                )
-            ),
-            synth=CodeBuildStep("CdkSynth_UnitTest",
+            synth=CodeBuildStep("CdkSynth",
                 input=source,
-                build_environment=codebuild.BuildEnvironment(
-                    build_image=codebuild.LinuxBuildImage.STANDARD_7_0
-                ),
                 install_commands=[
-                    "pip install -r requirements-dev.txt",
                     "pip install -r requirements.txt",
+                    "pip install -r requirements-dev.txt",
                     "npm install -g aws-cdk",
                 ],
                 commands=[
-                    "cdk synth -c stage=dev",
-                    # Unit test for CDK stack
-                    "python -m pytest",
-                    # Unit test for job scripts
-                    "WORKSPACE_LOCATION=$(pwd)/aws_glue_cdk_baseline/job_scripts/",
-                    "echo $WORKSPACE_LOCATION",
-                    "docker pull amazon/aws-glue-libs:glue_libs_4.0.0_image_01",
-                    "docker run -v ~/.aws:/home/glue_user/.aws -v $WORKSPACE_LOCATION:/home/glue_user/workspace/"
-                    " -e DISABLE_SSL=true --rm -p 4040:4040 -p 18080:18080"
-                    f" -e AWS_REGION={config['pipelineAccount']['awsAccountId']} -e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"
-                    " --name glue_pytest amazon/aws-glue-libs:glue_libs_4.0.0_image_01 -c \"python3 -m pytest\"",
+                    "cdk synth",
                 ],
-                role_policy_statements=[
-                    # S3 read only
-                    iam.PolicyStatement(
-                        actions=[
-                            "s3:ListBucket",
-                            "s3:GetObject"
-                        ],
-                        resources=["*"]
-                    ),
-                    # Glue read only
-                    iam.PolicyStatement(
-                        actions=[
-                            "glue:GetDatabase",
-                            "glue:GetDatabases",
-                            "glue:GetTable",
-                            "glue:GetTables",
-                            "glue:GetTableVersion",
-                            "glue:GetTableVersions",
-                            "glue:GetPartition",
-                            "glue:GetPartitions",
-                            "glue:BatchGetPartition",
-                            "glue:GetPartitionIndexes",
-                        ],
-                        resources=[
-                            "arn:aws:glue:*:*:catalog",
-                            "arn:aws:glue:*:*:database/*",
-                            "arn:aws:glue:*:*:table/*"
-                        ]
-                    )
-                ]
-            )
-        )
-        
-        # Dev deployment
-        dev_stage_name = "DeployDev"
-        if config["pipelineAccount"]["awsAccountId"] == config["devAccount"]["awsAccountId"] and config["pipelineAccount"]["awsRegion"] == config["devAccount"]["awsRegion"]:
-            dev_env = None
-        else:
-            dev_env = Environment(
-                account=str(config["devAccount"]["awsAccountId"]), 
-                region=config["devAccount"]["awsRegion"]
-            )
-        dev_stage_app = GlueAppStage(
-            self, 
-            dev_stage_name,
-            config=config,
-            stage="dev",
-            env=dev_env
-        )
-        dev_stage = pipeline.add_stage(dev_stage_app)
-
-
-        # Integ test
-        dev_stage.add_post(CodeBuildStep("IntegrationTest",
-                input=source,
                 build_environment=codebuild.BuildEnvironment(
                     build_image=codebuild.LinuxBuildImage.STANDARD_7_0
-                ),
-                install_commands=[
-                    "pip install -r requirements-dev.txt"
-                ],
-                commands=[
-                    # Integ test for Glue App stack
-                    f"python $(pwd)/tests/integ/integ_test_glue_app_stack.py --account {str(config['devAccount']['awsAccountId'])} --region {config['devAccount']['awsRegion']} --stage-name {dev_stage_name} --sts-role-arn {dev_stage_app.iam_role_arn}",
-                ],
-                role_policy_statements=[
-                    # Glue only
-                    iam.PolicyStatement(
-                        actions=[
-                            "sts:AssumeRole"
-                        ],
-                        resources=[
-                            "*"
-                        ]
-                    )
-                ]
+                )
             )
         )
+ 
+        # Add development stage
+        dev_stage = GlueAppStage(self, "DevStage", config=config, stage="dev", 
+            env=cdk.Environment(
+                account=str(config['devAccount']['awsAccountId']),
+                region=config['devAccount']['awsRegion']
+            ))
+        pipeline.add_stage(dev_stage)
 
-        # Prod deployment
-        prod_stage_name = "DeployProd"
-        if config["pipelineAccount"]["awsAccountId"] == config["prodAccount"]["awsAccountId"] and config["pipelineAccount"]["awsRegion"] == config["prodAccount"]["awsRegion"]:
-            prod_env = None
-        else:
-            prod_env = Environment(
-                account=str(config["prodAccount"]["awsAccountId"]), 
-                region=config["prodAccount"]["awsRegion"]
+        # Add production stage
+        prod_stage = GlueAppStage(self, "ProdStage", config=config, stage="prod", 
+            env=cdk.Environment(
+                account=str(config['prodAccount']['awsAccountId']),
+                region=config['prodAccount']['awsRegion']
+            ))
+        pipeline.add_stage(prod_stage)
+ 
+        # Glue Resource Sync as a separate step in the pipeline
+        pipeline.add_wave("GlueJobSync").add_post(CodeBuildStep("GlueJobSync",
+            input=source,
+            commands=[
+                "python $(pwd)/aws_glue_cdk_baseline/job_scripts/generate_mapping.py",
+                "python aws_glue_cdk_baseline/job_scripts/sync.py "
+                   "--dst-role-arn arn:aws:iam::{0}:role/GlueCrossAccountRole-prod "
+                   "--dst-region {1} "
+                   "--deserialize-from-file aws_glue_cdk_baseline/resources/resources.json "
+                   "--config-path mapping.json "
+                   "--targets job,catalog "
+                   "--skip-prompt".format(
+                       config['prodAccount']['awsAccountId'],
+                       config['prodAccount']['awsRegion']
+                   ),
+            ],
+            role_policy_statements=[
+                iam.PolicyStatement(
+                    actions=[
+                        "sts:AssumeRole",
+                    ],
+                    resources=["*"]
+                )
+            ],
+            build_environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_7_0
             )
-        prod_stage_app = GlueAppStage(
-            self, 
-            prod_stage_name,
-            config=config,
-            stage="prod",
-            env=prod_env
-        )
-        prod_stage = pipeline.add_stage(prod_stage_app)
-        prod_stage.add_pre(ManualApprovalStep("Approval"))
+        ))
